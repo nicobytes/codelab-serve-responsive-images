6349eb9a2ab35c1d8b427674f99b20901cc3e9db
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2019-05-05 18:07:27 -0400

    🚧 work

--- resize.js
+++ resize.js
@@ -4,7 +4,6 @@ const directory = './src/assets/images';
 
 fs.readdirSync(directory).forEach(file => {
   const name = file.split('.')[0];
-
   sharp(`${directory}/${file}`)
     .resize(450, 450) // width
     .toFile(`${directory}/${name}-small.jpg`);
